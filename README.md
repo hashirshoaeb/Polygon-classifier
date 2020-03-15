b8842525c546617c0dd83ada8250cd07e899a295
hashirshoaeb <hashirshoaeb@gmail.com>
2020-03-16 01:46:58 +0500

    adding images

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Polygon-classifier
 Distinguishing polygon shapes in testing image on the basis of provided training dataset using KNN algorithm.
+[1, 1, 4, 4, 2, 1, 1, 4, 2, 1, 1, 1, 1, 1]
--- assign.py
+++ assign.py
@@ -3,34 +3,34 @@ import cv2 as cv
 import blob as b
 import csv as cs
 import math as m
+import os
 
-# training images
-arrytemp1 = cv.imread("temp1.png", 0)
-arrytemp2 = cv.imread("temp2.png", 0)
-arrytemp3 = cv.imread("temp3.png", 0)
-arrytemp4 = cv.imread("temp4.png", 0)
-# testing image
-assignment = cv.imread("assignment1.png", 0)
+# importing training images
+training_imgs = []
+training_dir = './train'
+img_paths =  os.listdir(training_dir)
+for img_path in img_paths:
+    t = cv.imread(training_dir + '/' + img_path, 0)
+    training_imgs.append(t)
 
-# creating class objects
-obj1 = b.Blob()
-obj2 = b.Blob()
-obj3 = b.Blob()
-obj4 = b.Blob()
+# importing testing image
+test = cv.imread("test/test.png", 0)
 
-# extracting features from training images
-data1 = obj1.extractfeature(arrytemp1)
-data2 = obj2.extractfeature(arrytemp2)
-data3 = obj3.extractfeature(arrytemp3)
-data4 = obj4.extractfeature(arrytemp4)
-# function returning the array of features it extracted
-array1 = obj1.features()
-array2 = obj2.features()
-array3 = obj3.features()
-array4 = obj4.features()
+# extracting features
+features = []
+for img in training_imgs:
+    # creating class objects
+    obj = b.Blob()
+    # extracting features from training images
+    obj.extractfeature(img)
+    # function returning the array of features it extracted
+    f = obj.features()
+    print(f)
+    features.append(f)
+
 
 # writing features in csv, so that we do not need to train again
-csvinputdata = [array1, array2, array3, array4]
+csvinputdata = features
 with open('person.csv','w') as csvfile:
     writer=cs.writer(csvfile)
     writer.writerows(csvinputdata)
@@ -46,5 +46,5 @@ csvFile.close()
 print(csvoutputdata)
 
 res = b.Blob()
-resdata = res.findobjects(assignment,csvoutputdata)
+resdata = res.findobjects(test,csvoutputdata)
 print(resdata)
